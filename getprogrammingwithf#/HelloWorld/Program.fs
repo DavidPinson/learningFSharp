f704fe7a8b3e2e643b94cd622bbfa1b5b8be2936
SAURON\sideshowbob <pinson.david@gmail.com>
2024-10-06 16:03:43 -0400

    wip: still learning...

---
+++ getprogrammingwithf#/HelloWorld/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.Net.Http
+open System.Windows.Forms
+open System
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv = 
+    let items = argv.Length
+    printfn "Passed in %d items: %A" items argv
+
+    let httpClient = new HttpClient()
+    //let fsharpOrgContent = async {
+    //    let! data = httpClient.GetStringAsync("http://fsharp.org") |> Async.AwaitTask
+    //    return data
+    //} 
+
+    let fsharpOrgContent = httpClient.GetStringAsync("http://fsharp.org") |> Async.AwaitTask |> Async.RunSynchronously
+
+    let browser = new WebBrowser()
+    browser.ScriptErrorsSuppressed <- true
+    browser.Dock <- DockStyle.Fill
+    browser.DocumentText <- fsharpOrgContent
+
+    let richTextBox = new RichTextBox();
+    richTextBox.Dock <- DockStyle.Fill
+    richTextBox.Text <- fsharpOrgContent
+
+    let form = new Form()
+    //form.Controls.Add browser
+    form.Controls.Add richTextBox
+    form.Show()
+
+    printfn "%s" fsharpOrgContent
+
+    Console.ReadKey()
+
+    1
---
+++ getprogrammingwithf#/HelloWorld/scripts/ScriptREPL.fsx
@@ -0,0 +1,35 @@
+﻿open System
+
+let greeting = "Hello World"
+let askForName = "What is your name?"
+
+let greetPerson name age =
+    sprintf "Hello, %s. You are %d years young" name age
+
+let greeting2 = greetPerson "Fred" 25
+
+let add first second = first + second
+
+let result = add 2 3
+
+let foo =
+    let x = 10
+    printfn "%d" (10 + 20)
+    let x = "test"
+    let x = 50
+    x + 200
+
+let getLength (name:string) = sprintf "Name is %d letters." name.Length
+let foo2 (name) = "Hello! " + getLength name
+foo2 "bob"
+
+let sayHello someValue = 
+    let innerFunction number = 
+        if number > "Hello" then "Isaac"
+        elif number > 20 then "Fred"
+        else "Sara"
+    let resultOfInner = 
+        if someValue < 10.0 then innerFunction 5
+        else innerFunction 15
+    "Hello " + resultOfInner
+let result2 = sayHello 10.5
